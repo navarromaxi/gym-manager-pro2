93fa7c2fa94d140c0a2b8a9461b4042c252d47ca
Maximiliano Navarro <134528921+navarromaxi@users.noreply.github.com>
2025-09-19 19:37:58 -0300

    errores

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
+          <Toaster />
         </ThemeProvider>
         <SpeedInsights /> {/* 👈 Insertado correctamente para Vercel */}
       </body>
